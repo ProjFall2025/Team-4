69c2d2dc1b914928c82bc2ce5ceb843ebc146efc
Saunil3 <saunilpatel2403@gmail.com>
2025-12-07 15:39:12 -0500

    Refactor API base URL to use environment variable

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,13 +1,16 @@
 import axios from "axios";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${API_BASE_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
 export default api;
+
